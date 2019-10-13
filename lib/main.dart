81d84a440eba3ad079b35e55ce345686089af87c
leenow <47808807+leenow1218@users.noreply.github.com>
2019-10-13 23:12:18 +0900

    광고와 특정회차결과 화면에 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
+import 'dart:io';
+
+import 'package:ads/ads.dart';
 import 'package:flutter/material.dart';
 import 'dart:math';
+
+import 'package:lotto/provider/history_bloc.dart';
+import 'model/history_model.dart';
 
 void main() => runApp(MyApp());
 
@@ -26,32 +32,61 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<int> nums = [0, 0, 0, 0, 0, 0];
-  List<int> tempNums = [];
+  HistoryBloc _historyBloc;
+  List<int> _nums = [0, 0, 0, 0, 0, 0];
+  List<int> _tempNums = [];
   final _random = new Random();
+  int _episodeNum = 880;
+  Ads appAds;
+
+  final String appId = Platform.isAndroid
+      ? 'ca-app-pub-2558645202827085~8690630192'
+      : '';
+
+  final String bannerUnitId = Platform.isAndroid
+      ? 'ca-app-pub-2558645202827085/6996057533'
+      : '';
 
   int next(int min, int max) => min + _random.nextInt(max - min);
 
+
   @override
   void initState() {
+    appAds = Ads(
+      appId,
+      bannerUnitId: bannerUnitId,
+      childDirected: false,
+      testDevices: ['Samsung_Galaxy_SII_API_26:5554'],
+      testing: true,
+    );
+
+    appAds.showBannerAd();
+
+    _historyBloc = HistoryBloc(_episodeNum);
     _shackNumber();
   }
 
+  @override
+  void dispose() {
+    _historyBloc.dispose();
+    super.dispose();
+  }
+
   _shackNumber() {
-    tempNums = [];
+    _tempNums = [];
     for (int i = 0; i < 6; i++) {
       int randomNum = next(1, 45);
 
-      if (tempNums.indexOf(randomNum) == -1) {
-        tempNums.add(randomNum);
+      if (_tempNums.indexOf(randomNum) == -1) {
+        _tempNums.add(randomNum);
       } else {
         i = i - 1;
       }
     }
-    tempNums.sort();
+    _tempNums.sort();
 
     setState(() {
-      nums = tempNums;
+      _nums = _tempNums;
     });
   }
 
@@ -66,7 +101,41 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              '로또 당첨 번호',
+              '$_episodeNum회차 번호',
+              style: TextStyle(fontSize: 32.0),
+            ),
+            StreamBuilder<History>(
+              stream: _historyBloc.history$,
+              builder: (context, snapshot) {
+                print('${snapshot.data}');
+                return !snapshot.hasData
+                    ? CircularProgressIndicator()
+                    : Container(
+                        child: Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: Center(),
+                            ),
+                            _pickNumberWidget(snapshot.data.drwtNo1),
+                            _pickNumberWidget(snapshot.data.drwtNo2),
+                            _pickNumberWidget(snapshot.data.drwtNo3),
+                            _pickNumberWidget(snapshot.data.drwtNo4),
+                            _pickNumberWidget(snapshot.data.drwtNo5),
+                            _pickNumberWidget(snapshot.data.drwtNo6),
+                            Expanded(
+                              child: Center(),
+                            ),
+                          ],
+                        ),
+                      );
+              },
+            ),
+            Divider(
+              thickness: 2.0,
+              height: 48,
+            ),
+            Text(
+              '로또 행운 번호',
               style: TextStyle(fontSize: 32.0),
             ),
             Row(
@@ -74,25 +143,28 @@ class _MyHomePageState extends State<MyHomePage> {
                 Expanded(
                   child: Center(),
                 ),
-                _pickNumberWidget(nums[0]),
-                _pickNumberWidget(nums[1]),
-                _pickNumberWidget(nums[2]),
-                _pickNumberWidget(nums[3]),
-                _pickNumberWidget(nums[4]),
-                _pickNumberWidget(nums[5]),
+                _pickNumberWidget(_nums[0]),
+                _pickNumberWidget(_nums[1]),
+                _pickNumberWidget(_nums[2]),
+                _pickNumberWidget(_nums[3]),
+                _pickNumberWidget(_nums[4]),
+                _pickNumberWidget(_nums[5]),
                 Expanded(
                   child: Center(),
                 ),
               ],
-            )
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            FloatingActionButton(
+              onPressed: _shackNumber,
+              tooltip: 'Increment',
+              child: Icon(Icons.refresh),
+            ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _shackNumber,
-        tooltip: 'Increment',
-        child: Icon(Icons.refresh),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 
---
+++ lib/provider/history_bloc.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+
+import 'package:lotto/model/history_model.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:http/http.dart' as http;
+
+class HistoryBloc {
+  final _historySubject = BehaviorSubject<History>();
+
+  HistoryBloc(int episode) {
+    fetch(episode);
+  }
+
+  void fetch(int episode) async {
+    var history = await getHistoryData(episode);
+    _historySubject.add(history);
+  }
+
+  void dispose() {
+    _historySubject.close();
+  }
+
+  Stream<History> get history$ => _historySubject.stream;
+
+  final String baseUrl = 'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=';
+
+  Future<History> getHistoryData(int episode) async {
+    final response = await http.get('$baseUrl$episode');
+    if (response.statusCode == 200) {
+      return History.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load getHistoryData');
+    }
+  }
+}
+
